06fda0e47bdb99e7a00505f67f469d69e5f3b053
HelderSepu <HelderSepu@gmail.com>
2017-11-06 09:33:25 -0500

    Add a PagedListDocumentFilter
    It still not 100% what you need, but a big step in the right direction

--- swashbuckletest/Startup.cs
+++ swashbuckletest/Startup.cs
@@ -7,6 +7,10 @@
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Swashbuckle.AspNetCore.SwaggerGen;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using X.PagedList;   
 
     public class Startup
     {
@@ -29,6 +33,7 @@
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.DocumentFilter<PagedListDocumentFilter>();
             });
         }
 
@@ -50,7 +55,42 @@
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.DocExpansion("full");
             });
+        }
+
+        private class PagedListDocumentFilter : IDocumentFilter
+        {            
+            public void Apply(SwaggerDocument swaggerDoc, DocumentFilterContext context)
+            {
+                if (swaggerDoc.Definitions != null && swaggerDoc.Definitions.ContainsKey("Customer"))
+                {
+                    swaggerDoc.Definitions.Add("CustomerPagedList", PagedListSchema);
+                }
+                foreach (var path in swaggerDoc.Paths)
+                {
+                    if (path.Value.Get != null && path.Value.Get.Responses != null && path.Value.Get.Responses.ContainsKey("200"))
+                        if (path.Value.Get.Responses["200"].Schema.Items.Ref == "#/definitions/Customer")
+                            path.Value.Get.Responses["200"].Schema.Items.Ref = "#/definitions/CustomerPagedList";
+                }
+            }
+
+            private Schema PagedListSchema
+            {
+                get
+                {
+                    var data = new Dictionary<string, Schema>();
+                    var pagedCust = typeof(IPagedList);
+                    foreach (PropertyInfo property in pagedCust.GetTypeInfo().GetProperties())
+                    {
+                        var sch = new Schema();
+                        data.Add(property.Name, sch);
+                    }
+                    var s = new Schema { Type = "array", Items = new Schema { Ref = "#/definitions/Customer" } };
+                    data.Add("items", s);
+                    return new Schema { Properties = data };
+                }
+            }
         }
     }
 }

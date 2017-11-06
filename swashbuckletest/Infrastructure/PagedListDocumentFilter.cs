79e7fa44011a4fe9bd89be0cccc7d9724b5f24fd
Andre Muehle <andre.muehle@migrosluzern.ch>
2017-11-06 14:44:23 +0100

    fixed imports

--- swashbuckletest/Infrastructure/PagedListDocumentFilter.cs
+++ swashbuckletest/Infrastructure/PagedListDocumentFilter.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Reflection;
     using Swashbuckle.AspNetCore.Swagger;
     using Swashbuckle.AspNetCore.SwaggerGen;
     using X.PagedList;

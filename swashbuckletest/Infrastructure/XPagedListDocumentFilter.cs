ba087079ff99fef5e5306b30205493e5b1a633ec
Andre Muehle <andre.muehle@migrosluzern.ch>
2017-12-12 10:57:09 +0100

    working solution

---
+++ swashbuckletest/Infrastructure/XPagedListDocumentFilter.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using X.PagedList;
+
+namespace swashbuckletest.Infrastructure
+{
+    public class XPagedListDocumentFilter : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, DocumentFilterContext context)
+        {
+            List<string> models = FindPagedListModels(context);
+
+            if (models.Any())
+            {
+                ChangeOutputForPageListModels(swaggerDoc, models);
+            }
+        }
+
+        private void ChangeOutputForPageListModels(SwaggerDocument swaggerDoc, List<string> models)
+        {
+            var definitions = swaggerDoc.Definitions.Where(x => models.Contains(x.Key));
+
+            var newdefinitions = new Dictionary<string, Schema>();
+
+            foreach (var definition in definitions)
+            {
+                string modelName = definition.Key;
+
+                newdefinitions.Add($"{modelName}PagedList", GetPagedListSchema(modelName));
+
+                ChangePathsForModelName(swaggerDoc, modelName);
+            }
+
+            foreach (var def in newdefinitions)
+            {
+                swaggerDoc.Definitions.Add(def);
+            }
+        }
+
+        private void ChangePathsForModelName(SwaggerDocument swaggerDoc, string modelName)
+        {
+            foreach (var path in swaggerDoc.Paths)
+            {
+                if (path.Value.Get?.Responses != null &&
+                    path.Value.Get.Responses.ContainsKey("200") &&
+                    path.Value.Get.Responses["200"].Schema?.Items != null &&
+                    path.Value.Get.Responses["200"].Schema.Items.Ref == $"#/definitions/{modelName}")
+                {
+                    path.Value.Get.Responses["200"].Schema.Ref = $"#/definitions/{modelName}PagedList";
+                    path.Value.Get.Responses["200"].Schema.Type = null;
+                }
+            }
+        }
+
+        private List<string> FindPagedListModels(DocumentFilterContext context)
+        {
+            var models = new List<string>();
+
+            // api version 1
+            foreach (var versionGroup in context.ApiDescriptionsGroups.Items)
+            {
+                FindAllModelsFromGetResponses(models, versionGroup);
+            }
+
+            return models;
+        }
+
+        private void FindAllModelsFromGetResponses(List<string> models, ApiDescriptionGroup versionGroup)
+        {
+            // items in current API Version
+            foreach (ApiDescription description in GetAllGetMethods(versionGroup))
+            {
+                var response = description.SupportedResponseTypes.SingleOrDefault(x => x.StatusCode == 200 && x.Type.Namespace.ToLower().Contains("x.pagedlist"));
+
+                if (response != null)
+                {
+                    models.Add(response.Type.GenericTypeArguments.First().Name);
+                }
+            }
+        }
+
+        private IEnumerable<ApiDescription> GetAllGetMethods(ApiDescriptionGroup versionGroup)
+        {
+            return versionGroup.Items.Where(x => x.HttpMethod.ToLower() == "get");
+        }
+
+        private Schema GetPagedListSchema(string modelName)
+        {
+            var properties = new Dictionary<string, Schema>();
+            var pagedCust = typeof(IPagedList);
+
+            foreach (PropertyInfo property in pagedCust.GetTypeInfo().GetProperties())
+            {
+                var schema = new Schema();
+                properties.Add(property.Name.FirstCharToLowerCase(), schema);
+            }
+
+            var itemsSchema = new Schema {Type = "array", Items = new Schema {Ref = $"#/definitions/{modelName}"}};
+            properties.Add("items", itemsSchema);
+
+            return new Schema {Properties = properties};
+        }
+    }
+}
--- swashbuckletest/Startup.cs
+++ swashbuckletest/Startup.cs
@@ -1,16 +1,11 @@
 ﻿namespace swashbuckletest
 {
     using Infrastructure;
-    using Newtonsoft.Json;
     using Swashbuckle.AspNetCore.Swagger;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using Swashbuckle.AspNetCore.SwaggerGen;
-    using System.Collections.Generic;
-    using System.Reflection;
-    using X.PagedList;   
 
     public class Startup
     {
@@ -24,16 +19,12 @@
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddControllersAsServices().AddJsonOptions(options =>
-            {
-                options.SerializerSettings.NullValueHandling = NullValueHandling.Include;
-                options.SerializerSettings.Converters.Add(new CustomIEnumerableConverter(new[] { "X.PagedList" }));
-            });
+            services.AddMvc();
 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
-                c.DocumentFilter<PagedListDocumentFilter>();
+                c.DocumentFilter<XPagedListDocumentFilter>();
             });
         }
 
@@ -57,48 +48,6 @@
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
                 c.DocExpansion("full");
             });
-        }
-
-        private class PagedListDocumentFilter : IDocumentFilter
-        {            
-            public void Apply(SwaggerDocument swaggerDoc, DocumentFilterContext context)
-            {
-                if (swaggerDoc.Definitions != null && swaggerDoc.Definitions.ContainsKey("Customer"))
-                {
-                    swaggerDoc.Definitions.Add("CustomerPagedList", PagedListSchema);
-                }
-
-                foreach (var path in swaggerDoc.Paths)
-                {
-                    if (path.Value.Get != null && path.Value.Get.Responses != null && path.Value.Get.Responses.ContainsKey("200"))
-                    { 
-                        if (path.Value.Get.Responses["200"].Schema.Items.Ref == "#/definitions/Customer")
-                        { 
-                            path.Value.Get.Responses["200"].Schema.Items.Ref = "#/definitions/CustomerPagedList";
-                        }
-                    }
-                }
-            }
-
-            private Schema PagedListSchema
-            {
-                get
-                {
-                    var properties = new Dictionary<string, Schema>();
-                    var pagedCust = typeof(IPagedList);
-
-                    foreach (PropertyInfo property in pagedCust.GetTypeInfo().GetProperties())
-                    {
-                        var schema = new Schema();
-                        properties.Add(property.Name.FirstCharToLowerCase(), schema);
-                    }
-
-                    var customerSchema = new Schema { Type = "array", Items = new Schema { Ref = "#/definitions/Customer" } };
-                    properties.Add("items", customerSchema);
-
-                    return new Schema { Properties = properties };
-                }
-            }
         }
     }
 }
